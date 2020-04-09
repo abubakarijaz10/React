70a7c0537fe247f2277dd9954c3bb32d81f3040d
Abubakar Ijaz <55194821+abubakarijaz10@users.noreply.github.com>
2020-04-09 19:21:02 +0500

    Add files via upload

---
+++ AbubakarIjaz.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class AbubakarIjaz extends Component {
+	render(){
+		return(
+			<div>
+			<html><h1>Abubakar Ijaz</h1><a href="https://youtu.be/dQw4w9WgXcQ">CLICK HERE FOR 100 BITCOINS !!!</a></html>
+			</div>
+	);}
+}
+
+export default AbubakarIjaz;
